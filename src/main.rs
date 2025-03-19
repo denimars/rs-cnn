c215080b662f0ace871c1c0bcefe0fa64b43c821
denimarswnadi <denimarswandi@gmail.com>
2025-03-20 03:24:41 +0800

    + data

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,29 @@ use nn::Linear;
 use matrix::Matrix;
 use model::Model;
 use activation::Activation;
+use std::fs::File;
+use std::io::{self, BufReader, Read};
+
+fn load_mnist_as_matrix(filepath: &str) -> io::Result<Matrix> {
+    let file = File::open(filepath)?;
+    let mut reader = BufReader::new(file);
+    let mut buffer = Vec::new();
+    reader.read_to_end(&mut buffer)?;
+    let num_images = buffer.len()/(784 * 4);
+    let mut data = Vec::with_capacity(num_images * 784);
+
+    for chunk in buffer.chunks( 4) {
+        if chunk.len() == 4{
+            let value = f32::from_be_bytes(chunk.try_into().unwrap());
+            data.push(value);
+        }
+    }
+    Ok(Matrix::new(data, num_images, 784))
+
+}
 
 fn main() {
-    let x = Matrix::random(10, 5, -1.0, 1.0);
-    let layers = vec![
-        Linear::new(5, 10, Activation::RELU),
-        Linear::new(10, 2, Activation::RELU),
-
-    ];
-    let model = Model::new(layers);
-
-    let output = model.forward(&x);
-    println!("x=\n{x}");
-
-    println!("{output}");
+    let x_train = load_mnist_as_matrix("data/x_train.bin").unwrap();
+    println!("num_images={}", x_train.rows);
+    println!("dim={}", x_train.cols)
 }
